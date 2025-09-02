a60a0691ebfd532fcadd152074d97f1a821abf65
ArdeshirV <e-job@protonmail.com>
2025-09-02 17:01:36 +0200

    Add CalculateHash

--- main.go
+++ main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 )
 
 func main() {
 	fmt.Println(Prompt("My Blockchain"))
+}
 
+type Block struct {
+  Index int
+  TimeStamp string
+  Data string
+  Hash string
+  PrevHash string
+}
+
+type Blockchain struct {
+  Chain []Block
+}
+
+func CalculateHash(block Block) string {
+  record := fmt.Sprintf("%d%s%s%s", block.Index, block.TimeStamp,
+    block.Data, block.PrevHash)
+  h := sha256.New()
+  h.Write([]byte(record))
+  hashed := h.Sum(nil)
+  return hex.EncodeToString(hashed)
 }
